71a954922032bf59c1d480a0bef72e0adcb30994
vas <vas77794@gmail.com>
2026-02-14 09:36:42 +0100

    s1t4: state

---
+++ auth-service/tests/api/helpers.rs
@@ -0,0 +1,36 @@
+use auth_service::Application;
+
+pub struct TestApp {
+    pub address: String,
+    pub http_client: reqwest::Client,
+}
+
+impl TestApp {
+    pub async fn new() -> Self {
+        let app = Application::build("127.0.0.1:0")
+            .await
+            .expect("Failed to build app");
+
+        let address = format!("http://{}", app.address.clone());
+
+        // Run the auth service in a separate async task
+        // to avoid blocking the main test thread. 
+        #[allow(clippy::let_underscore_future)]
+        let _ = tokio::spawn(app.run());
+
+        let http_client = todo!(); // Create a Reqwest http client instance
+
+        // Create new `TestApp` instance and return it
+        todo!()
+    }
+
+    pub async fn get_root(&self) -> reqwest::Response {
+        self.http_client
+            .get(&format!("{}/", &self.address))
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
+
+    // TODO: Implement helper functions for all other routes (signup, login, logout, verify-2fa, and verify-token)
+}
