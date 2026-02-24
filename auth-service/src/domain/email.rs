411e9af8b6108658b098be73cdfe71b89a113ab0
vas <vas77794@gmail.com>
2026-02-24 22:31:53 +0100

    s2t4: state property testing

--- auth-service/src/domain/email.rs
+++ auth-service/src/domain/email.rs
@@ -23,11 +23,43 @@ impl AsRef<str> for Email {
 mod test {
 
     use super::Email;
+
+    use fake::faker::internet::en::SafeEmail;
+    use fake::Fake;
+    use quickcheck::Gen;
+    use rand::SeedableRng;
+
     #[test]
     fn check_email() {
-        assert!(Email::parse("vas@gmial.com".to_owned()).is_ok());
-        assert!(Email::parse("@gmial.com".to_owned()).is_err());
-        assert!(Email::parse("".to_owned()).is_err());
-        // let email = Email("".to_owned());
+        // let email = Email();
+        // assert_eq!(
+        //     Email::parse("vas@gmail.com".to_owned()),
+        //     Ok(Email("vas@gmail.com".to_owned()))
+        // );
+        // println!("{email:?}");
+        // let email = Email("vasgmial.com".to_owned());
+        // assert_eq!(email.parse(), Err("Not valid email vasgmial.com".to_owned()));
+        // let email = Email("vasgmial.com".to_owned());
+        // assert_eq!(email.parse(), Err("Not valid email vasgmial.com".to_owned()));
+        // let email = Email("vasgmial.com".to_owned());
+        // assert_eq!(email.parse(), Ok(()));
     }
+
+    #[derive(Debug, Clone)]
+    struct ValidEmailFixture(pub String);
+
+    impl quickcheck::Arbitrary for ValidEmailFixture {
+        fn arbitrary(g: &mut Gen) -> Self {
+            let seed: u64 = u64::arbitrary(g);
+            let mut rng = rand::rngs::SmallRng::seed_from_u64(seed);
+            let email = SafeEmail().fake_with_rng(&mut rng);
+            Self(email)
+        }
+    }
+
+    #[quickcheck_macros::quickcheck]
+    fn valid_emails_are_parsed_successfully(valid_email: ValidEmailFixture) -> bool {
+        Email::parse(valid_email.0).is_ok()
+    }
+
 }
